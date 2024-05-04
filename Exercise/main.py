1ef17426c06591889c8954b9e20b5e79b4c80598
YMD <94185468+J4cky1028@users.noreply.github.com>
2024-05-05 02:04:07 +0800

    Add files via upload

---
+++ Exercise/main.py
@@ -0,0 +1,3 @@
+from realjail import pyjail
+
+pyjail()
---
+++ Exercise/realjail.py
@@ -0,0 +1,18 @@
+import unicodedata
+
+blacklist = ['import', 'os', 'system', 'eval', 'exec', 'lambda',
+             'help', 'dict', 'dir', 'input', 'locals', 'compile',
+             'bool', 'any', 'ascii', '__builtins__', '__class__',
+             '[', ']']
+
+def pyjail():
+    print('Welcome to HKUST!')
+    print('What do you want to do now?')
+    print('What? you want to escape?!!i!!11!l!!11!')
+    method = input('How?\n')
+    method = unicodedata.normalize('NFKC', method)
+    for word in blacklist:
+        if word in method:
+            print('You can\'t escape from HKUST, you GG.')
+            return
+    eval(method)
